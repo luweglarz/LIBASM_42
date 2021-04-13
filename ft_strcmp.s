48760f4c9fc9b8208bee380b024fb8dfbe2bce02
RolandGamos <lucasweglarz9@gmail.com>
2021-04-13 22:48:14 +0200

    ft_strcmp.s

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -6,21 +6,27 @@
 #    By: user42 <user42@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/12 16:22:02 by user42            #+#    #+#              #
-#    Updated: 2021/04/12 16:40:44 by user42           ###   ########.fr        #
+#    Updated: 2021/04/13 17:15:03 by user42           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 global ft_strcmp
 
 ft_strcmp:
-
-loop:
+	mov al, byte [rdi]
 	mov cl, byte [rsi]
-	cmp byte [rdi], cl
-	jz ret
+	cmp al, 0
+	je comp
+	cmp cl, 0
+	je comp
+	cmp al, cl
+	je comp
 	inc rsi
 	inc rdi
+	jmp ft_strcmp
+
+comp:
+	sub al, cl
 
 ret :
-	mov rax, je
-
+	ret
--- ft_strcpy.s
+++ ft_strcpy.s
@@ -6,25 +6,21 @@
 #    By: user42 <user42@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/09 16:48:18 by user42            #+#    #+#              #
-#    Updated: 2021/04/12 17:47:16 by user42           ###   ########.fr        #
+#    Updated: 2021/04/12 17:52:03 by user42           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 global ft_strcpy
 
 ft_strcpy:
-	push rdi
-
-loop:
 	cmp byte [rsi], 0
 	je ret
 	mov cl, byte [rsi]
 	mov byte [rdi], cl
 	inc rdi
 	inc rsi
-	jmp loop	
+	jmp ft_strcpy
 
 ret:
 	mov byte [rdi], 0
-	pop rax
 	ret
